88836f4e5ef3b3faf1c83daea20dca88a32a4e2f
Lucas Horand <lucas.horand@gmail.com>
2015-03-31 15:45:41 +0200

    Fixed that f*cking error
    used fetch instead of fetchAll
    
    GOD IM SO DUMB

--- model/Corpse.php
+++ model/Corpse.php
@@ -5,25 +5,19 @@ class Corpse extends Model{
 	function getRandCorpseId(){
 			$login = $this->db->prepare("SELECT idCorpse FROM ".$this->table."");
 			$login->execute();
-			$result = $login->fetch(PDO::FETCH_ASSOC);
+			$result = $login->fetchAll(PDO::FETCH_ASSOC);
 			echo "un corps au pif : ";
 			print_r($result);
 		
 	}
 
-	//THAT FUCKER ONLY TAKES THE FIRST ONE I HATE IT.
-	//fuck
-
-	//fuck fuck fuck
-
-	//really, fuck.
 	function getRandItemId($type, $number){
 		$table = "ce_".strtolower($type);
 		$idName = "id".$type;
 
 		$result = $this->db->prepare("SELECT ".$idName." FROM ".$table);
 		$result->execute();
-		$result = $result->fetch(PDO::FETCH_NUM);
+		$result = $result->fetchAll(PDO::FETCH_ASSOC);
 		echo count($result);
 		print_r($result);
 
