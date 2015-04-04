4be187399019dbdb6eb6e0d9f92f533abc7201bf
Lucas Horand <lucas.horand@gmail.com>
2015-04-04 15:12:02 +0200

    updated database

--- localhost-22-03-2015.sql
+++ localhost-22-03-2015.sql
@@ -3,13 +3,82 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Mar 31 Mars 2015 à 14:27
+-- Généré le :  Sam 04 Avril 2015 à 15:11
 -- Version du serveur :  5.5.38
 -- Version de PHP :  5.6.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+--
+-- Base de données :  `User`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `roomid` int(11) NOT NULL,
+  `roomname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `rooms`
+--
+
+INSERT INTO `rooms` (`roomid`, `roomname`) VALUES
+(1, 'roomOne'),
+(2, 'roomTwo');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `password` int(11) NOT NULL,
+  `roomid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `password`, `roomid`) VALUES
+(1, 'jsklfmjqm', 1, 1),
+(2, 'zafzfafa', 2, 2);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `rooms`
+--
+ALTER TABLE `rooms`
+ ADD PRIMARY KEY (`roomid`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`roomid`);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`roomid`) REFERENCES `rooms` (`roomid`);
 --
 -- Base de données :  `cadavre`
 --
@@ -27,7 +96,7 @@ CREATE TABLE `ce_action` (
   `objectx` int(11) DEFAULT NULL,
   `objecty` int(11) DEFAULT NULL,
   `url_thumb` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `ce_action`
@@ -35,7 +104,9 @@ CREATE TABLE `ce_action` (
 
 INSERT INTO `ce_action` (`idAction`, `name`, `shortAction`, `objectx`, `objecty`, `url_thumb`) VALUES
 (1, 'Mangea', 1, 10, 10, 'eat.png'),
-(2, 'Explosa', 1, 10, 10, 'explode.png');
+(2, 'Explosa', 1, 10, 10, 'explode.png'),
+(3, 'questionna', 1, 30, 10, 'questionna.png'),
+(4, 'Vomit', 1, 10, 10, 'vomit.png');
 
 -- --------------------------------------------------------
 
@@ -48,14 +119,16 @@ CREATE TABLE `ce_character` (
   `name` varchar(45) DEFAULT NULL,
   `url` varchar(45) DEFAULT NULL,
   `url_thumb` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `ce_character`
 --
 
 INSERT INTO `ce_character` (`idCharacter`, `name`, `url`, `url_thumb`) VALUES
-(1, 'Charlie Darraud', 'charlie.png', 'charlie_small.png');
+(1, 'Charlie Darraud', 'charlie.png', 'charlie_small.png'),
+(2, 'un clown', 'clown.png', 'clown_small.png'),
+(3, 'Funland', 'funland.png', 'funland_small.png');
 
 -- --------------------------------------------------------
 
@@ -70,7 +143,14 @@ CREATE TABLE `ce_corpse` (
   `idPlace` int(11) NOT NULL,
   `likesCount` int(11) NOT NULL,
   `corpse_by` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `ce_corpse`
+--
+
+INSERT INTO `ce_corpse` (`idCorpse`, `finished`, `img`, `idPlace`, `likesCount`, `corpse_by`) VALUES
+(1, 0, '', 1, 0, 'luhof2');
 
 -- --------------------------------------------------------
 
@@ -94,7 +174,7 @@ CREATE TABLE `ce_object` (
   `name` varchar(45) DEFAULT NULL,
   `url` varchar(45) DEFAULT NULL,
   `url_thumb` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `ce_object`
@@ -102,7 +182,10 @@ CREATE TABLE `ce_object` (
 
 INSERT INTO `ce_object` (`idObject`, `name`, `url`, `url_thumb`) VALUES
 (1, 'Chaussette', 'sock.png', 'sock_small.png'),
-(2, 'Chien', 'dog.png', 'dog_small.png');
+(2, 'Chien', 'dog.png', 'dog_small.png'),
+(3, 'Monstre hideux', 'monster.png', 'monster_small.png'),
+(4, 'Smartphone', 'smartphone.png', 'smartphone_small.png'),
+(5, 'Sandwich', 'sandwich.png', 'sandwich_small.png');
 
 -- --------------------------------------------------------
 
@@ -128,19 +211,20 @@ CREATE TABLE `ce_panel` (
 
 CREATE TABLE `ce_place` (
 `idPlace` int(11) NOT NULL,
-  `idCorpse` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `url` varchar(50) NOT NULL,
   `url_thumb` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `ce_place`
 --
 
-INSERT INTO `ce_place` (`idPlace`, `idCorpse`, `name`, `url`, `url_thumb`) VALUES
-(3, 1, 'Dans la forêt', 'forest.png', 'forest.png'),
-(4, 2, 'Dans la rue', 'street.png', 'street.png');
+INSERT INTO `ce_place` (`idPlace`, `name`, `url`, `url_thumb`) VALUES
+(3, 'Dans la forêt', 'forest.png', 'forest.png'),
+(4, 'Dans la rue', 'street.png', 'street.png'),
+(5, 'à l''IMAC', 'imac.png', 'imac_small.png'),
+(6, 'Sommet de la tour eiffel', 'eiffel.png', 'eiffel_small.png');
 
 -- --------------------------------------------------------
 
@@ -252,7 +336,7 @@ ALTER TABLE `ce_panel`
 -- Index pour la table `ce_place`
 --
 ALTER TABLE `ce_place`
- ADD PRIMARY KEY (`idPlace`), ADD KEY `idCorpse` (`idCorpse`);
+ ADD PRIMARY KEY (`idPlace`);
 
 --
 -- Index pour la table `ce_user`
@@ -274,22 +358,22 @@ ALTER TABLE `ce_userinfo`
 -- AUTO_INCREMENT pour la table `ce_action`
 --
 ALTER TABLE `ce_action`
-MODIFY `idAction` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `idAction` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `ce_character`
 --
 ALTER TABLE `ce_character`
-MODIFY `idCharacter` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `idCharacter` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `ce_corpse`
 --
 ALTER TABLE `ce_corpse`
-MODIFY `idCorpse` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `idCorpse` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `ce_object`
 --
 ALTER TABLE `ce_object`
-MODIFY `idObject` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `idObject` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `ce_panel`
 --
@@ -299,7 +383,7 @@ MODIFY `idCase` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT pour la table `ce_place`
 --
 ALTER TABLE `ce_place`
-MODIFY `idPlace` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `idPlace` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `ce_user`
 --
@@ -318,8 +402,8 @@ MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
 -- Contraintes pour la table `ce_likes`
 --
 ALTER TABLE `ce_likes`
-ADD CONSTRAINT `ce_likes_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `ce_user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `ce_likes_ibfk_2` FOREIGN KEY (`idCorpse`) REFERENCES `ce_place` (`idCorpse`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `ce_likes_ibfk_2` FOREIGN KEY (`idCorpse`) REFERENCES `ce_corpse` (`idCorpse`),
+ADD CONSTRAINT `ce_likes_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `ce_user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `ce_panel`
