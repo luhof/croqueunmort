5d5caaa47f13341762c232e273fe8fb8ce423a0e
Lucas Horand <lucas.horand@gmail.com>
2015-04-06 00:52:55 +0200

    drawing works !
    maintenant il faut update le nom de fichier dans la table corpse
    
    et faire en sorte que tout soit dessiné :/

--- utilities/draw.php
+++ utilities/draw.php
@@ -8,9 +8,17 @@
 
 	function createImg($id){
 
+		$fullImg = imagecreatetruecolor(660, 215);
+		imagealphablending($fullImg , false); // updated to FALSE
+		imagesavealpha($fullImg , true);
+
 		$folder = ROOT.DS."webroot".DS."images/corpses/";
 		$img = imagecreatefrompng($folder."default.png");
-		imagepng($img, $folder."corpse_".$id.".png");
+
+		imagecopy($fullImg, $img, 0, 0, 0, 0, 660, 215);
+
+		imagepng($fullImg, $folder."corpse_".$id.".png");
+		imagedestroy($fullImg);
 		imagedestroy($img);
 	}
 
@@ -19,17 +27,25 @@
 		$baseImg = ROOT.DS."webroot".DS."images/corpses/corpse_".$idCorpse.".png";
 		$backgroundFolder = ROOT.DS."assets".DS."images".DS."backgrounds".DS;
 
+		$fullImg = imagecreatetruecolor(660, 215);
+		imagealphablending($fullImg , false); // updated to FALSE
+		imagesavealpha($fullImg , true);
+
 		$img = imagecreatefrompng($baseImg);
 		$background = imagecreatefrompng($backgroundFolder.$background);
 
 
+		imagecopy($fullImg, $img, 0, 0, 0, 0, 660, 215);
 		/*add the same background for every image*/
 		/*with an offset of $i*220 because every panel is 220px width*/
 		for($i=0; $i<3; $i++){
-			imagecopy($img, $background, $i*220, 0, 0, 0, 220, 215);
+			imagecopy($fullImg, $background, $i*220, 0, 0, 0, 220, 215);
 		}
 
-		imagepng($img, $baseImg);
+
+		imagepng($fullImg, $baseImg);
+
+		imagedestroy($fullImg);
 		imagedestroy($img);
 		imagedestroy($background);
 	}
@@ -38,14 +54,19 @@
 		$baseImg = ROOT.DS."webroot".DS."images/corpses/corpse_".$idCorpse.".png";
 		$charactersFolder = ROOT.DS."assets".DS."images".DS."characters".DS;
 
+		$fullImg = imagecreatetruecolor(660, 215);
+		imagealphablending($fullImg , false); // updated to FALSE
+		imagesavealpha($fullImg , true);
+
 		$img = imagecreatefrompng($baseImg);
 		$character = imagecreatefrompng($charactersFolder.$character);
 
-		imagecopy($img, $character, $step*220, 0, 0, 0, 220, 215);
+		imagecopy($fullImg, $img, 0, 0, 0, 0, 660, 215);
+		imagecopy($fullImg, $character, $step*220, 0, 0, 0, 220, 215);
 		//imagecopymerge($img, $character, $step*220, 0, 0, 0, 220, 215, 100);
 		//($img, $character, $step*220, 0, 0, 0, 220, 215);
 
-		imagepng($img, $baseImg);
+		imagepng($fullImg, $baseImg);
 		imagedestroy($img);
 		imagedestroy($character);
 	}
